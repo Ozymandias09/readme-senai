134d358751a6847b0bd97abefe912a508544fb73
Marcos Batista <batista.vinicius09@gmail.com>
2022-03-14 11:02:23 -0300

    Update Senai Atv01.js

--- Senai Atv01.js
+++ Senai Atv01.js
@@ -1,38 +1,28 @@
+//Data
+var dataAtual = new Date(2021,08,30);
 
-let data;
-let idade;
-let num_participantes;
-let qtd_participantes;
-let cadastro=0;
+var dataEvento = new Date(2021,09,12);
 
-let num_participantes = prompt("Digite o numero de participantes");
-if(num_participantes>=100){
-  console.log("Continuar cadastro");
-  cadastro=num_participantes;
-}else{
-  console.log("o cadastro não será permitido por ter excedido o limite.")
+if (dataEvento > dataAtual) {
+    console.log("Evento permitido")
+} else {
+    console.log("Data inválida")
 }
 
-let data = prompt("Digite a data");
-if(data>=23){
-  console.log("Continuar cadastro");
-  cadastro=data;
-}else{
-  console.log("o cadastro não será permitido por ter excedido a data limite.")
+// Idade
+var idade = 27;
+
+if(idade >= 18) {
+    console.log("Cadastro permitido")
+} else {
+    console.log("Cadastro não permitido para menores de 18 anos")
 }
 
-let idade = prompt("Digite a idade do participante");
-if(idade>=18){
-  console.log("Continuar cadastro");
-  cadastro=num_participantes;
-}else{
-  console.log("o cadastro não será permitido por ter excedido o limite de idade.")
-}
-let qtd_participantes = prompt("Digite o numero de participantes");
-if(contador>=100){
-  console.log("Continuar cadastro");
-  cadastro=data;
-}else{
-  console.log("o cadastro não será permitido por ter excedido a data limite de participantes.")
-}
+// Participantes
+var listaParticipantes = ["Joao", "Sandra", "Joelma"]
 
+if (listaParticipantes.length < 100) {
+    console.log ("Cadastro concluído com sucesso")
+} else {
+    console.log ("Cadastro não permitido. O número de participantes excedeu o limite")
+}
